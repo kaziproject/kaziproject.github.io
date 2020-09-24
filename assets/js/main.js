a6b2b52f34f38c9a4b646db5cf49dcc92be230b8
Ngugisenior <ngugijoseph992@gmail.com>
2020-09-24 16:53:56 +0200

    kazi web init

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const home = document.getElementById('home');
+const register = document.getElementById('register');
+const companyView = document.getElementById('company');
+const main = document.getElementById('main-content');
+
+companyView.addEventListener('click', companyV);
+register.addEventListener('click', registerStud);
+//Company View
+function companyV(){
+    main.innerHTML = `
+    <iframe class="airtable-embed" src="https://airtable.com/embed/shr6UuVuzN2ipB6N7?backgroundColor=green&layout=card&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="1000" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    `;
+};
+
+function registerStud(){
+    main.innerHTML=`
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrkx03RGPiF9yW1g?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="1000" style="background: transparent; border: 1px solid #ccc;"></iframe>
+    `;
+};
