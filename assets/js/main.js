0818257d5c798e1ce4bc809b6c0b0e139b3f010f
Ngugisenior <ngugijoseph992@gmail.com>
2020-09-24 17:53:52 +0200

    iframe

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,12 +8,12 @@ register.addEventListener('click', registerStud);
 //Company View
 function companyV(){
     main.innerHTML = `
-    <iframe class="airtable-embed" src="https://airtable.com/embed/shr6UuVuzN2ipB6N7?backgroundColor=green&layout=card&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="1000" style="background: transparent;"></iframe>
+    <iframe class="airtable-embed" src="https://airtable.com/embed/shr6UuVuzN2ipB6N7?backgroundColor=green&layout=card&viewControls=on" frameborder="0" onmousewheel="" width="100%" height="700" style="background: transparent;"></iframe>
     `;
 };
 
 function registerStud(){
     main.innerHTML=`
-    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrkx03RGPiF9yW1g?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="1000" style="background: transparent;"></iframe>
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shrkx03RGPiF9yW1g?backgroundColor=teal" frameborder="0" onmousewheel="" width="100%" height="700" style="background: transparent;"></iframe>
     `;
 };
